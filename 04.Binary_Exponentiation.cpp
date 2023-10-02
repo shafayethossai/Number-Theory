7b3274925aad1d9d50b91435fff570b259f34305
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2023-10-02 22:40:44 +0600

    Update 04.Binary_Exponentiation.cpp

--- 04.Binary_Exponentiation.cpp
+++ 04.Binary_Exponentiation.cpp
@@ -18,20 +18,19 @@ long long power_recursion (long long n, long long m) {
 }
 
 // ************************************Binary_Exponentiation_Manually*****************
-long long power_manually (long long base, long long m) {
-    long long result = 1;
-
-    while (base) {
-        if (m % 2 == 1) {
-            result = result * base;
-            m--;
-        }
-        else {
-            base = base * base;
-            m = m/2;
-        }
-    }
-    return result;
+ll power_manually(ll base,ll n,ll mod){
+	ll result=1;
+	while(n){
+		if(n%2==1){
+			result = (result*base)%mod;
+			n--;
+		}
+		else {
+			base=(base*base)%mod;
+			n/=2;
+		}
+	}
+	return result;
 }
 
 int main () {
@@ -41,7 +40,8 @@ int main () {
     while (tc--) {
         long long n, m;
         cin >> n >> m;
-        cout << power_manually(n, m) << endl;
+        cout<<power_recursion(n, m) << endl;
+        cout<<power_manually(n, m, 1e9)<<endl;
     }
     return 0;
 }
