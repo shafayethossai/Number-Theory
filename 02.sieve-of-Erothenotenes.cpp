9a37530a112052c2c908ab3862230edc89928894
Shafayet Hossain <128078040+shafayethossai@users.noreply.github.com>
2024-11-12 11:12:16 +0600

    Update 02.sieve-of-Erothenotenes.cpp

--- 02.sieve-of-Erothenotenes.cpp
+++ 02.sieve-of-Erothenotenes.cpp
@@ -3,6 +3,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Using Bitset
+
+// const int N = 1e8;
+// bitset<N> is_prime;
+// vector<int> v;
+
+// void sieve() {
+//     is_prime[0] = is_prime[1] = true;
+
+//     for (int i = 2; i * i < N; i++) {
+//         if (!is_prime[i]) {
+//             for (int j = i * i; j < N; j += i) {
+//                 is_prime[j] = true;
+//             }
+//         }
+//     }
+
+//     for (int i = 2; i < N; i++) {
+//         if (!is_prime[i]) {
+//             v.push_back(i);
+//         }
+//     }
+// }
+
 void sieve (int n) {
     int prime[n+3];
     memset(prime, 0, sizeof(prime));
